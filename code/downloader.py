c6eb922fafb0a8e75075481fc9ac2df3484954f2
maxwildcat <maxwildcat@gmail.com>
2019-10-30 16:52:55 +0200

    added check if container in x
    sometimes container is not found (very quick and dirty but works locally) 
    also someone please check /test 
    we could also have a try catch although superfluous

--- code/downloader.py
+++ code/downloader.py
@@ -137,9 +137,11 @@ class Downloader(object):
             raise Exception('Failed to fetch video meta')
 
         for x in meta_res.json()['sources']:
-            if x['container'] == 'MP4' and 'src' in x:
-                dl_url = x['src']
-                break
+            # sometimes container is not found (very quick and dirty but works) - also someone please check /test 
+            if 'container' in x:
+                if x['container'] == 'MP4' and 'src' in x:
+                    dl_url = x['src']
+                    break
 
         print('Downloading {}...'.format(fpath))
 
